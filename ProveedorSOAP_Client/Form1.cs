8b6230dfd7d72c6132ec9a0c78f1e4cd77806fa9
Ronny Rodman Navarro Laguna <IPRonnyNavarro@hotmail.com>
2022-03-18 01:44:09 -0600

    Creación de Métodos y Clientes

---
+++ ProveedorSOAP_Client/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProveedorSOAP_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region
+        ProveedorServiceReference.ProveedorWebService1SoapClient ws = new ProveedorServiceReference.ProveedorWebService1SoapClient();
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ws.GetProveedor();
+        }
+    }
+}
--- SOAPBD.Web/ProveedorWebService1.asmx.cs
+++ SOAPBD.Web/ProveedorWebService1.asmx.cs
@@ -18,18 +18,106 @@ namespace SOAPBD.Web
     public class ProveedorWebService1 : System.Web.Services.WebService
     {
 
-        [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hola a todos";
-        }
-
-        [WebMethod(Description = "Devuelve la lista de Proveedor")]
+        [WebMethod(Description = "Devuelve la lista de Proveedores")]
         public List<proveedor> GetProveedor()
         {
             BDSOAPEntities conexion = new BDSOAPEntities();
             var consulta = (from c in conexion.proveedores select c).ToList();
             return consulta;
         }
+
+        /// <summary>
+        /// Método para consultar un proveedor
+        /// </summary>
+        /// <param name="nombre">nombre tipo string</param>
+        /// <returns>clase proveedor</returns>
+        [WebMethod(Description = "Devuelve la un Proveedor")]
+        public proveedor ObtenerProveedor(string nombre)
+        {
+            BDSOAPEntities conexion = new BDSOAPEntities();
+            var consulta = (from c in conexion.proveedores where c.nombre == nombre select c).FirstOrDefault();
+            return consulta;
+        }
+
+        [WebMethod(Description = "Agregar un Proveedor")]
+        public bool AgregarProveedor(string Nombre, string Direccion, String Telefono, String Email)
+        {
+            try
+            {
+                using (var conexion = new BDSOAPEntities())
+                {
+                    proveedor proveedornuevo = new proveedor();
+                    proveedornuevo.nombre = Nombre;
+                    proveedornuevo.direccion = Direccion;
+                    proveedornuevo.telefono = Telefono;
+                    proveedornuevo.email = Email;
+                    conexion.proveedores.Add(proveedornuevo);
+                    conexion.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        [WebMethod(Description = "Eliminar un Proveedor")]
+        public bool BorrarProveedor(int id)
+            {
+            try
+            {
+                using (var conexion = new BDSOAPEntities())
+                {
+                    var consulta = (from c in conexion.proveedores where c.id == id select c).FirstOrDefault();
+                    if(consulta != null)
+                    {
+                        conexion.proveedores.Remove(consulta);
+                        conexion.SaveChanges();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        [WebMethod(Description = "Modificar un Proveedor")]
+        public bool ActualizarProveedor(int id, string nombre, string direccion, string telefono, string email)
+        {
+            try
+            {
+                using (var conexion = new BDSOAPEntities())
+                {
+                    var consulta = (from c in conexion.proveedores where c.id == id select c).FirstOrDefault();
+                    if (consulta != null)
+                    {
+                        consulta.nombre = nombre;
+                        consulta.direccion = direccion;
+                        consulta.telefono = telefono;
+                        consulta.email = email;
+                        conexion.SaveChanges();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
     }
 }
