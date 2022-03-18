6d935ff08712f352cca633088a25623ddfe42a66
Ronny Rodman Navarro Laguna <IPRonnyNavarro@hotmail.com>
2022-03-18 02:17:33 -0600

    Diseño de FORMS

--- ProveedorSOAP_Client/Form1.Designer.cs
+++ ProveedorSOAP_Client/Form1.Designer.cs
@@ -45,13 +45,14 @@ namespace ProveedorSOAP_Client
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(333, 423);
+            this.button1.Location = new System.Drawing.Point(379, 458);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 44);
             this.button1.TabIndex = 0;
@@ -61,8 +62,9 @@ namespace ProveedorSOAP_Client
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(313, 106);
+            this.dataGridView1.Location = new System.Drawing.Point(359, 141);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(698, 287);
             this.dataGridView1.TabIndex = 1;
@@ -70,7 +72,7 @@ namespace ProveedorSOAP_Client
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(692, 423);
+            this.button2.Location = new System.Drawing.Point(738, 458);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(124, 44);
             this.button2.TabIndex = 2;
@@ -81,7 +83,7 @@ namespace ProveedorSOAP_Client
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(511, 423);
+            this.button3.Location = new System.Drawing.Point(557, 458);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(124, 44);
             this.button3.TabIndex = 3;
@@ -91,20 +93,22 @@ namespace ProveedorSOAP_Client
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(871, 423);
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(917, 458);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(124, 44);
             this.button4.TabIndex = 4;
             this.button4.Text = "ELIMINAR";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(53, 44);
+            this.label1.Location = new System.Drawing.Point(132, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(936, 29);
             this.label1.TabIndex = 5;
@@ -114,7 +118,7 @@ namespace ProveedorSOAP_Client
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(55, 170);
+            this.label2.Location = new System.Drawing.Point(101, 205);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 16);
             this.label2.TabIndex = 6;
@@ -124,7 +128,7 @@ namespace ProveedorSOAP_Client
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(49, 230);
+            this.label3.Location = new System.Drawing.Point(95, 265);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 16);
             this.label3.TabIndex = 7;
@@ -134,7 +138,7 @@ namespace ProveedorSOAP_Client
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(51, 294);
+            this.label4.Location = new System.Drawing.Point(97, 329);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(70, 16);
             this.label4.TabIndex = 8;
@@ -144,7 +148,7 @@ namespace ProveedorSOAP_Client
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(62, 367);
+            this.label5.Location = new System.Drawing.Point(108, 402);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(47, 16);
             this.label5.TabIndex = 9;
@@ -152,28 +156,28 @@ namespace ProveedorSOAP_Client
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(146, 170);
+            this.textBox1.Location = new System.Drawing.Point(192, 205);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 10;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(146, 230);
+            this.textBox2.Location = new System.Drawing.Point(192, 265);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 11;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(146, 294);
+            this.textBox3.Location = new System.Drawing.Point(192, 329);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 12;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(146, 367);
+            this.textBox4.Location = new System.Drawing.Point(192, 402);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 13;
@@ -182,7 +186,7 @@ namespace ProveedorSOAP_Client
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(74, 115);
+            this.label6.Location = new System.Drawing.Point(120, 150);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(23, 16);
             this.label6.TabIndex = 14;
@@ -190,16 +194,28 @@ namespace ProveedorSOAP_Client
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(146, 115);
+            this.textBox5.Location = new System.Drawing.Point(192, 150);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(736, 541);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(305, 72);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "NOTA: SOLO para Modificar\r\n y Eliminar se tiene que insertar\r\n obligatoriamente e" +
+    "l ID.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1289, 643);
+            this.ClientSize = new System.Drawing.Size(1175, 643);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox4);
@@ -217,7 +233,7 @@ namespace ProveedorSOAP_Client
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Proveedores";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -242,6 +258,7 @@ namespace ProveedorSOAP_Client
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
     }
 }
 
